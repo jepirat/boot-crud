2f935dad8518ed2b5fc3a82e2c2953ef02b476f9
jeka <evgeshakev@gmail.com>
2021-09-26 16:42:36 +0400

    вроде бы годно к проверке

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -1,73 +1,79 @@
-//package web.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import web.model.Role;
-//import web.model.User;
-//import web.repos.UserRepo;
-//
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//
-//@Controller
-//public class MainController {
-//    @Autowired
-//    UserRepo userRepo;
-//    @GetMapping("/index")
-//    public String test(Model model, Authentication authentication) {
-//        User user = (User) authentication.getPrincipal();
-//        if (user.getRoles().contains(new Role("ADMIN"))) {
-//            model.addAttribute("status", "ADMIN");
-//        } else {
-//            model.addAttribute("status", "USER");
-//        }
-//        model.addAttribute("currentUser", user);
-//        List<User> users = userRepo.findAll();
-//        model.addAttribute("userList", users);
-//        model.addAttribute("user", new User());
-//        Role[] roles = {new Role("ADMIN"), new Role("USER")};
-//        model.addAttribute("list",roles);
-//        return "index";
-//    }
-//
-//    @PostMapping("/index")
-//    public String testPost(@RequestParam(name = "id", required = false) Long id,
-//                           @RequestParam(name = "login", required = false) String login,
-//                           @RequestParam(name = "fn", required = false) String fn,
-//                           @RequestParam(name = "ln", required = false) String ln,
-//                           @RequestParam(name = "pass", required = false) String pass,
-//                           @RequestParam(name = "role", required = false) String role,
-//                           @RequestParam(name = "action", required = false) String action) {
-//        System.out.println(role);
-//        Set<Role> roles = new HashSet<>();
-//        if (id != null) {
-//            roles = userRepo.findById(id).get().getRole();
-//            if (role != null && role.equals("ADMIN")) {
-//                roles.add(new Role(role));
-//            } else if (role != null && role.equals("USER")) {
-//                System.out.println("удаление роли");
-//                roles.remove(new Role("ADMIN"));
-//            }
-//        }
-//        roles.add(new Role("USER"));
-//        User user = new User();
-//        user.setId(id);
-//        user.setLogin(login);
-//        user.setFirstName(fn);
-//        user.setLastName(ln);
-//        user.setPassword(pass);
-//        user.setRoles(roles);
-//        if (action.equals("delete")) {
-//            userRepo.delete(user);
-//        } else {
-//            userRepo.save(user);
-//        }
-//        return "redirect:/index";
-//    }
-//}
+package web.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.Role;
+import web.model.User;
+import web.repos.RoleRepo;
+import web.repos.UserRepo;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/index")
+public class MainController {
+    UserRepo userRepo;
+    RoleRepo roleRepo;
+
+    public MainController(UserRepo userRepo, RoleRepo roleRepo) {
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+    }
+
+    @GetMapping
+    public String usersController(Model model) {
+        List<User> users = userRepo.findAll();
+        model.addAttribute("userList", users);
+        Role[] roles = {new Role("ADMIN"), new Role("USER")};
+        model.addAttribute("list",roles);
+        return "index";
+    }
+
+    @PostMapping
+    public String add(@ModelAttribute("user") User user) {
+        Role role = new Role("USER");
+        roleRepo.save(role);
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        user.setRoles(roles);
+        userRepo.save(user);
+        return "redirect:/index";
+    }
+
+
+    @PatchMapping
+    public String update(@ModelAttribute("user") User user,
+                         @RequestParam(name = "role", required = false) String role) {
+
+        System.out.println(user);
+        System.out.println(role);
+        Set<Role> roles = userRepo.findById(user.getId()).get().getRole();
+        if(roles == null) {
+            roles = new HashSet<>();
+        }
+        if (role != null && role.equals("ADMIN")) {
+            Role role3 = new Role(role);
+            roleRepo.save(role3);
+            roles.add(role3);
+        } else if (role != null && role.equals("USER")) {
+            if (roles.contains(new Role("ADMIN"))) {
+                roles.stream().forEach(rol -> {if (rol.getName().equals("ADMIN")) {
+                    roleRepo.delete(rol);}});
+            }
+            Role role1 = new Role(role);
+            roleRepo.save(role1);
+            roles.add(role1);
+            roles.removeIf(role2 -> role2.getName().equals("ADMIN"));
+        }
+        user.setRoles(roles);
+        userRepo.save(user);
+        return "redirect:/index";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userRepo.delete(userRepo.findById(id).get());
+        return "redirect:/index";
+    }
+}
